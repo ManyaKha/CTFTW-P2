2ae0d294eea7ffb7eeea2c0643827bbdb3ef2154
karendaa <karendaa@stud.ntnu.no>
2021-12-04 13:26:33 +0100

    add frontend client project and fix project name

---
+++ Frontend/CLIENT8902/src/main/java/domains/User.java
@@ -0,0 +1,72 @@
+package domains;
+
+public class User {
+
+	private String email;
+	private String password;
+	private String name;
+	private String surname;
+	private String city;
+	private boolean administrator;
+	
+	public User() {
+	}
+	
+	public User(String email, String password, String name, String surname, String city, boolean administrator) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.surname = surname;
+		this.city = city;
+		this.administrator = administrator;
+	}
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public boolean isAdministrator() {
+		return administrator;
+	}
+	public void setAdministrator(boolean administrator) {
+		this.administrator = administrator;
+	}
+
+	
+	
+}

f4b538d24863fce67bd017cfca9c315ac3a449af
solveiglangas <solveig.langas@hotmail.com>
2021-12-07 13:30:59 +0100

    admin frontend init commit

---
+++ Frontend/ADMIN8902/src/main/java/es/uc3m/tiw/Admin8902Application.java
@@ -0,0 +1,13 @@
+package es.uc3m.tiw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Admin8902Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Admin8902Application.class, args);
+	}
+
+}

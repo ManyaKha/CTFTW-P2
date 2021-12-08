6c812b6bf5dd53c4b3e18897a9c787a5b44e6f7f
ManyaKha <60505955+ManyaKha@users.noreply.github.com>
2021-12-08 12:36:11 +0100

    Catalog Microservice

--- Microservices/CATAL8902/src/main/java/es/uc3m/tiw/catalogController.java
+++ Microservices/CATAL8902/src/main/java/es/uc3m/tiw/catalogController.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,21 +31,47 @@ public class catalogController {
 	@Autowired
 	RestTemplate restTemplate;
 	
+	/*Main Page*/
    @RequestMapping(value = "/index")
    public String index() {
       return "index";
    }
    
-  /** @RequestMapping(value="/products", method = RequestMethod.GET)
-	public @ResponseBody ResponseEntity<List<Product>> getUsers(@RequestParam(value="Title",required=false) String Title){
-		List<Product> productList;
-		productList = daoproduct.findAll();
-		
-		return new ResponseEntity<>(productList, HttpStatus.OK);
-	}**/
-   
+   /*View all Products*/
    @RequestMapping(value= "/products",method = RequestMethod.GET)
-	public @ResponseBody List<Product> getUsers(){
+	public @ResponseBody List<Product> getAllProducts(){
 		return daoproduct.findAll();
 	}
+   
+   /*Search Product by Title*/
+   @RequestMapping(value= "/products/{title}",method = RequestMethod.GET)
+   public @ResponseBody List<Product> getProductsByTitle(String title){
+		return daoproduct.findByTitle(title);
+   }
+	
+   /*Search Product by Owner*/
+   @RequestMapping(value= "/products/{owner}",method = RequestMethod.GET)
+   public @ResponseBody List<Product> getProductsByOwner(String owner){
+		return daoproduct.findByOwner(owner);
+   }
+   
+   /*Search Product by Title and Owner*/
+   @RequestMapping(value= "/products/{title}/{owner}",method = RequestMethod.GET)
+   public @ResponseBody Product getProductsByTitleAndOwner(String title, String owner){
+		return daoproduct.findByTitleAndOwner(title,owner);
+   }
+   
+   /*Search Product by Category*/
+   @RequestMapping(value= "/products/{category}",method = RequestMethod.GET)
+   public @ResponseBody List<Product> getProductByCategory(String category){
+		return daoproduct.findByCategory(category);
+   }
+  
+   
+   /*Search Product by Status*/
+   @RequestMapping(value= "/products/{status}",method = RequestMethod.GET)
+   public @ResponseBody List<Product> getProductByStatus(String status){
+		return daoproduct.findByStatus(status);
+   }
+   
 }
--- Microservices/CATAL8902/src/main/java/es/uc3m/tiw/domains/Product.java
+++ Microservices/CATAL8902/src/main/java/es/uc3m/tiw/domains/Product.java
@@ -6,14 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name="product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	String title;
 
 	String owner;

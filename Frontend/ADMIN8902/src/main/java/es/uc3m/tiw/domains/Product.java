cd0d91383670a5a0c35d7d48b32fd3831cfdd0f3
solveiglangas <solveig.langas@hotmail.com>
2021-12-09 17:02:04 +0100

    product domain

---
+++ Frontend/ADMIN8902/src/main/java/es/uc3m/tiw/domains/Product.java
@@ -0,0 +1,78 @@
+package es.uc3m.tiw.domains;
+
+import java.io.Serializable;
+
+
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	String title;
+	String owner;
+	String category;
+	String description;
+	int price;
+	String status;
+	
+	public Product() {
+	}
+	
+	public Product(String title, String owner, String category, String description, int price, String status) {
+		this.title = title;
+		this.owner = owner;
+		this.category = category;
+		this.description = description;
+		this.price = price;
+		this.status = status;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}		
+	
+	
+}

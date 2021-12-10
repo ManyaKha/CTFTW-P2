5bc2a55b0c5a46ccf87356bd7334748fc578d0f0
solveiglangas <solveig.langas@hotmail.com>
2021-12-10 16:31:09 +0100

    product price from int to float

--- CTFTW_project2_script.sql
+++ CTFTW_project2_script.sql
@@ -15,15 +15,15 @@ CREATE TABLE USER (
 );
 
 CREATE TABLE PRODUCT (
-	`ProductId` varchar(36) NOT NULL,
+	`Id` varchar(36) NOT NULL,
 	`Title` varchar(100) NOT NULL,
 	`Owner` varchar(45) NOT NULL,
 	`Category` varchar(45) NOT NULL,
 	`Description` varchar(500) DEFAULT NULL,
 	`Image` longblob DEFAULT NULL,
-	`Price` int(45) NOT NULL,
+	`Price` float(45) NOT NULL,
 	`Status` varchar(45) NOT NULL,
-	PRIMARY KEY (ProductId),
+	PRIMARY KEY (Id),
 	FOREIGN KEY (Owner) REFERENCES USER(email)
 );
 
@@ -33,36 +33,29 @@ INSERT INTO `User` (Email, Password, Name, Surname, City, Administrator) VALUES 
 INSERT INTO `User` (Email, Password, Name, Surname, City, Administrator) VALUES ('phil.ruiz@gmail.com', 'password', 'Philip', 'Ruiz', 'Madrid', true);
 INSERT INTO `User` (Email, Password, Name, Surname, City, Administrator) VALUES ('fergus-bishop@outlook.com', 'password', 'Fergus', 'Bishop', 'Madrid', false);
 
-INSERT INTO `Product` (ProductId, Title, Owner, Category, Description, Image, Price, Status) VALUES (
+INSERT INTO `Product` (Id, Title, Owner, Category, Description, Image, Price, Status) VALUES (
 '0343ae28-97fe-49bb-8f69-0499c9c256d3', 'Barbie house', 'lois.hull@gmail.com', 'toys', 
-'Im selling my kids barbie house since they have grown out of it and dont 
-like barbies anymore. It has been used for many years, but is still in good condition. 
-I hope this house can find some new ownersso it can continue to bring joy to others 
-as it has given my little girls. Price can be discussed.',
+'Im selling my kids barbie house since they have grown out of it and dont like barbies anymore. It has been used for many years, but is still in good condition. I hope this house can find some new ownersso it can continue to bring joy to others as it has given my little girls. Price can be discussed.',
 null, 20, 'available');
 
-INSERT INTO `Product` (ProductId, Title, Owner, Category, Description, Image, Price, Status) VALUES (
+INSERT INTO `Product` (Id, Title, Owner, Category, Description, Image, Price, Status) VALUES (
 '2ed4b054-b5d0-42e1-9df6-dedf462a60ff', 'Swimming feet', 'kathryn.irwin@hotmail.com', 'sport', 
 'Adult swimming feet. Good condition. Only used a couple of times.',
-null, 8, 'available');
+null, 8.5, 'available');
 
-INSERT INTO `Product` (ProductId, Title, Owner, Category, Description, Image, Price, Status) VALUES (
+INSERT INTO `Product` (Id, Title, Owner, Category, Description, Image, Price, Status) VALUES (
 'bad63485-fba5-44ae-9b89-2917e71ca068', 'Prom dress', 'isla-buckley@outlook.com', 'clothing', 
-'The prom dress did not fit me so Im selling it. I have only tried it on so it is brand new.
-I bought it from zalando and is selling it for half the price. Good deal!',
+'The prom dress did not fit me so Im selling it. I have only tried it on so it is brand new. I bought it from zalando and is selling it for half the price. Good deal!',
 null, 75, 'available');
 
-INSERT INTO `Product` (ProductId, Title, Owner, Category, Description, Image, Price, Status) VALUES (
+INSERT INTO `Product` (Id, Title, Owner, Category, Description, Image, Price, Status) VALUES (
 '88d145c3-42a8-4dca-84a4-675bb9dcaf42', 'Race car', 'phil.ruiz@gmail.com', 'cars', 
-'Selling a broken race car. Buy your fixer-upper dream or sell it for parts!
-Only sky is the limit with this race car. Selling it really cheap. Best deal you
-will get. Send me a mail for dsicussion of pick up.',
+'Selling a broken race car. Buy your fixer-upper dream or sell it for parts! Only sky is the limit with this race car. Selling it really cheap. Best deal you will get. Send me a mail for dsicussion of pick up.',
 null, 100, 'available');
 
-INSERT INTO `Product` (ProductId, Title, Owner, Category, Description, Image, Price, Status) VALUES (
+INSERT INTO `Product` (Id, Title, Owner, Category, Description, Image, Price, Status) VALUES (
 '4e80d939-ff1a-46d1-9998-43b7cf69b6ac', 'New tent', 'fergus-bishop@outlook.com', 'sport', 
-'Pacific Pass Tienda de campaña resistente al agua portátil con sobretecho. Original price
-was 39 from amazon.', 
+'Pacific Pass Tienda de campaña resistente al agua portátil con sobretecho. Original price was 39 from amazon.', 
 null, 20, 'available');
 
 

3edcf8433f619d3c568242fd5591138132fec457
solveiglangas <solveig.langas@hotmail.com>
2021-12-15 20:22:41 +0100

    removed unused mapping

--- Microservices/BANK8902/src/main/java/es/uc3m/tiw/PaymentController.java
+++ Microservices/BANK8902/src/main/java/es/uc3m/tiw/PaymentController.java
@@ -3,7 +3,6 @@ package es.uc3m.tiw;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
